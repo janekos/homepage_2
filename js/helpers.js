502b5c42cd578758f0a6d25fcad2dc7c87b06a59
Janek K <janek.kossinski@gmail.com>
2017-12-26 00:43:30 +0200

    nav improvements

--- js/helpers.js
+++ js/helpers.js
@@ -1,3 +1,20 @@
-let grab = function(id){
-    return document.getElementById(id);
+let grab = function(e){
+    let se = e.substr(1);
+    if(e.charAt(0) === "#"){        
+        return document.getElementById(se);   
+    }else if(e.charAt(0) === "."){
+        return document.getElementsByClassName(se);
+    }
+}
+
+let applyEventListenerClass = function(elArr, event, callback){
+    for(let element of elArr){
+        element.addEventListener(event, callback);   
+    }    
+}
+
+let applyFnClass = function(elArr, callback){
+    for(let element of elArr){
+        callback(element);   
+    }
 }
--- js/main.js
+++ js/main.js
@@ -5,14 +5,25 @@ class Main{
     }
     
     navCallback(e){
-        grab("menuBtn").classList.toggle("closedNav");
-        grab("navItems").classList.toggle("closedNav");
+        grab("#nav").classList.toggle("openedNav");
+        applyFnClass(grab(".closedNavElem"), function(element){
+            console.log(element.id);
+            element.classList.toggle("openedNav");
+        });
+        applyFnClass(grab(".openNavElem"), function(element){
+            console.log(element.id);
+            element.classList.toggle("openedNav");
+        });
     }
     
     bindEvents(){
-        grab("menuBtn").addEventListener("click", this.navCallback);
+        grab("#openNav").addEventListener("click", this.navCallback);
         
-        grab("closeNav").addEventListener("click", this.navCallback);
+        grab("#closeNav").addEventListener("click", this.navCallback);
+        
+        /*applyEventListenerClass(grab(".navItem"), "click", (e)=>{
+            console.log(e.target.dataset.href);
+        });*/
     }
 }
 
